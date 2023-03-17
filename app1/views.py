4a86f02a0e9240a8919f4177216bda016ea05852
Narendra5907 <narendranare5907@gmail.com>
2023-03-17 10:06:02 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dini(request):
+    return render(request,'dini.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def nare(request):
+    return render(request,'nare.html')
